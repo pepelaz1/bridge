716e36b9c966f6e5265533c4a58fc4df7a5cbd3d
pepelaz <pepelaz101@gmail.com>
2022-06-01 17:32:17 +0700

    changed hash to EIP-712

--- contracts/Bridge.sol
+++ contracts/Bridge.sol
@@ -16,8 +16,8 @@ contract Bridge {
     enum Operation {Swap, Redeem}
 
     event BridgeOperation(
-        address from,
-        address to,
+        address indexed from,
+        address indexed to,
         uint256 amount, 
         uint256 nonce,
         uint256 chainTo,
@@ -31,7 +31,7 @@ contract Bridge {
     }
 
     function swap(address _to, uint256 _amount, uint256 _nonce, uint256 _chainTo) public {
-        bytes32 hash = keccak256(abi.encodePacked(msg.sender, _to, _amount, _nonce, _chainTo));
+        bytes32 hash = getHash(msg.sender, _to, _amount, _nonce, _chainTo);
         require(processedHashes[msg.sender][hash] == false, 'already processed');
         processedHashes[msg.sender][hash] = true;
         token.burn(msg.sender, _amount);
@@ -40,7 +40,7 @@ contract Bridge {
 
    function redeem(address _from, address _to, uint256 _amount, uint256 _chainTo, uint256 _nonce, bytes calldata _signature) public {
         require(_chainTo == block.chainid, 'wrong chainId');
-        bytes32 hash = keccak256(abi.encodePacked(_from, _to, _amount, _nonce, _chainTo));       
+        bytes32 hash = getHash(_from, _to, _amount, _nonce, _chainTo);
         require(verify(hash, _signature, _from) , 'wrong signature');
         require(processedHashes[_from][hash] == false, 'already processed');
         processedHashes[_from][hash] = true;
@@ -50,6 +50,19 @@ contract Bridge {
 
     function verify(bytes32 _data, bytes calldata _signature, address _address) private pure returns (bool) {
         return _data.toEthSignedMessageHash().recover(_signature) == _address;
+    }
+
+    function getHash(address _from, address _to, uint256 _amount, uint256 _nonce, uint256 _chainTo) private pure returns(bytes32) {
+        //return keccak256(abi.encodePacked(_from, _to, _amount, _nonce, _chainTo));
+        bytes32 eip712DomainHash = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(address _from,address _to,uint256 _nonce,uint256 _amount,uint256 _chainTo)"
+                ),
+                _from, _to, _nonce,_amount, _nonce, _chainTo
+            )
+        );
+        return keccak256(abi.encodePacked(uint16(0x1901), eip712DomainHash));
     }
 
 }
